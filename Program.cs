ed2307d72751004e0391d277fea281b02f2365c4
OptoCloud <26223094+OptoCloud@users.noreply.github.com>
2023-06-02 21:49:44 +0200

    Inline some processing

--- Program.cs
+++ Program.cs
@@ -22,110 +22,96 @@ var imports = new List<IImportable>()
 // Get path to temp folder, and create sqlite database
 string tempPath = Path.GetTempPath();
 string dbPath = Path.Combine(tempPath, "OptoPacker.db");
-// Delete old database if it exists
-if (File.Exists(dbPath))
-{
-    File.Delete(dbPath);
-}
-// Create new database
-var options = new DbContextOptionsBuilder<OptoPackerContext>()
-    .UseSqlite($"Data Source={dbPath}")
-    .Options;
-
-int i = 0;
-int cursorPos;
-Dictionary<string, PathTreeFile> files;
-List<int> prevLines = new List<int>();
-var hashPathDict = new Dictionary<string, string>();
-using (var context = new OptoPackerContext(options))
-{
-    context.Database.EnsureCreated();
-
-    Console.WriteLine("Gathering files...");
-    var importFiles = imports.Select(x => new PathTree(x.BasePath).AddMany(x.GetFiles())).ToArray();
-
-    Console.WriteLine("Creating directory database entries...");
-    var levels = new List<FlattenedPathTreeLevel>();
-    foreach (var importFile in importFiles)
-    {
-        await importFile.DbCreateDirectoriesAsync(context);
-    }
-
-
-    Console.WriteLine("Hashing files... (This might take a couple minutes)");
-    cursorPos = Console.CursorTop;
-    files = importFiles.SelectMany(x => x.GetAllFiles()).ToDictionary(x => x.OriginalPath);
-    var dbBlobs = new Dictionary<string, BlobEntity>();
-    var fileBlobPairs = new List<(PathTreeFile file, BlobEntity blob)>();
-    await foreach (ProcessedFileInfo file in ImportProcessor.ProcessFilesAsync(files.Keys.ToArray(), printStatusReport, hashingBlockSize, ChunkSize, ParallelTasks))
-    {
-        string hash = Utilss.BytesToHex(file.Hash);
-
-        // Get or queue blob
-        if (!dbBlobs.TryGetValue(hash, out BlobEntity? blob))
-        {
-            blob = new BlobEntity()
-            {
-                Hash = file.Hash,
-                Size = file.Size,
-            };
-            dbBlobs.Add(hash, blob);
-            hashPathDict.Add(hash, file.Path);
-        }
-        fileBlobPairs.Add((files[file.Path], blob));
-        i++;
-    }
-
-    Console.WriteLine("Writing blobs to database...");
-    await context.Blobs.AddRangeAsync(dbBlobs.Values.ToArray());
-    await context.SaveChangesAsync();
-
-    Console.WriteLine("Assigning file blobIds...");
-    var dbFiles = new List<FileEntity>();
-    foreach (var (file, blob) in fileBlobPairs)
-    {
-        string name = file.Name;
-        int extPos = name.LastIndexOf('.');
-
-        dbFiles.Add(new FileEntity
-        {
-            BlobId = blob.Id,
-            DirectoryId = file.DirectoryId!.Value,
-            Name = extPos > 0 ? name[..extPos] : name,
-            Extension = extPos > 0 ? name[(extPos + 1)..] : string.Empty,
-            Blob = blob,
-        });
-    }
-
-    Console.WriteLine("Writing files to database...");
-    await context.Files.AddRangeAsync(dbFiles);
-    await context.SaveChangesAsync();
-    await context.Database.CloseConnectionAsync();
-}
-// Stupid fix
-SqliteConnection.ClearAllPools();
-GC.Collect();
-GC.WaitForPendingFinalizers();
-
 string archivePath = "D:\\archive.7z";
+
+int cursorPos;
+uint filesComplete = 0;
+ulong bytesProcessed = 0;
+List<int> prevLines = new List<int>();
+Dictionary<string, PathTreeFile> files;
+
 // Start the 7-Zip process
 using (var process = new Process())
 {
     process.StartInfo.FileName = "7z"; // Path to 7z executable
-    process.StartInfo.Arguments = $"a -t7z \"{archivePath}\" -si\"data.tar\" -mx=9 -m0=lzma2 -aoa";
+    process.StartInfo.Arguments = $"a -t7z \"{archivePath}\" -si\"data.tar\" -mx=5 -m0=lzma2 -aoa";
     process.StartInfo.RedirectStandardInput = true;
     process.StartInfo.UseShellExecute = false;
     process.Start();
 
     using (var tarWriter = new TarWriter(process.StandardInput.BaseStream))
     {
-        // Iterate over the hashPathDict and add entries to the tar archive
-        foreach (var item in hashPathDict)
+        // Delete old database if it exists
+        if (File.Exists(dbPath))
         {
-            string hash = item.Key;
-            string filePath = item.Value;
-            tarWriter.WriteEntry(filePath, $"files/{hash[..2]}/{hash}");
+            File.Delete(dbPath);
         }
+        // Create new database
+        var options = new DbContextOptionsBuilder<OptoPackerContext>()
+            .UseSqlite($"Data Source={dbPath}")
+            .Options;
+
+        using (var context = new OptoPackerContext(options))
+        {
+            context.Database.EnsureCreated();
+
+            Console.WriteLine("Gathering files...");
+            var importFiles = imports.Select(x => new PathTree(x.BasePath).AddMany(x.GetFiles())).ToArray();
+
+            Console.WriteLine("Creating directory database entries...");
+            var levels = new List<FlattenedPathTreeLevel>();
+            foreach (var importFile in importFiles)
+            {
+                await importFile.DbCreateDirectoriesAsync(context);
+            }
+
+            Console.WriteLine("Hashing files... (This might take a couple minutes)");
+            cursorPos = Console.CursorTop;
+            files = importFiles.SelectMany(x => x.GetAllFiles()).ToDictionary(x => x.OriginalPath);
+            await foreach (ProcessedFileInfo file in ImportProcessor.ProcessFilesAsync(files.Keys.ToArray(), printStatusReport, hashingBlockSize, ChunkSize, ParallelTasks))
+            {
+                string hash = Utilss.BytesToHex(file.Hash);
+
+                var tarWriterTask = tarWriter.WriteEntryAsync(file.Path, $"files/{hash[..2]}/{hash}");
+
+                var blob = await context.Blobs.FirstOrDefaultAsync();
+                if (blob == null)
+                {
+                    blob = new BlobEntity()
+                    {
+                        Hash = file.Hash,
+                        Size = file.Size,
+                    };
+                    await context.Blobs.AddAsync(blob);
+                    await context.SaveChangesAsync();
+                }
+
+                var filee = files[file.Path];
+                string name = filee.Name;
+                int extPos = name.LastIndexOf('.');
+
+                await context.Files.AddAsync(new FileEntity
+                {
+                    BlobId = blob.Id,
+                    DirectoryId = filee.DirectoryId!.Value,
+                    Name = extPos > 0 ? name[..extPos] : name,
+                    Extension = extPos > 0 ? name[(extPos + 1)..] : string.Empty,
+                    Blob = blob,
+                });
+                await context.SaveChangesAsync();
+
+                await tarWriterTask;
+                filesComplete++;
+                bytesProcessed += file.Size;
+            }
+
+            await context.SaveChangesAsync();
+            await context.Database.CloseConnectionAsync();
+        }
+        // Stupid fix
+        SqliteConnection.ClearAllPools();
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
 
         // Add the database to the archive
         tarWriter.WriteEntry(dbPath, "index.db");
@@ -135,11 +121,13 @@ using (var process = new Process())
 }
 File.Delete(dbPath);
 
+Console.WriteLine("Done!");
+
 void printStatusReport(MultiFileStatusReport summary)
 {
     printStatus(cursorPos, "Status:",
-        $"   Processed {summary.filesProcessed} / {files.Count} Files",
-        $"   Processed {Utilss.FormatNumberByteSize(summary.bytesProcessed)} / {Utilss.FormatNumberByteSize(summary.bytesTotal)} Bytes",
+        $"   {summary.filesProcessed} / {files.Count} files hashed ({Utilss.FormatNumberByteSize(summary.bytesProcessed)} / {Utilss.FormatNumberByteSize(summary.bytesTotal)})",
+        $"   {filesComplete} / {files.Count} files archived ({Utilss.FormatNumberByteSize(bytesProcessed)} / {Utilss.FormatNumberByteSize(summary.bytesTotal)})",
         $"   Average files size: {Utilss.FormatNumberByteSize(summary.bytesTotal / summary.filesTotal)}"
         );
 }
@@ -166,8 +154,6 @@ void printStatus(int basePos, string title, params string[] lines)
 
     Console.Write(sb.ToString());
 }
-
-Console.WriteLine($"Done, processed {i} / {files.Count} files");
 
 sealed record FlattenedPathTreeLevel(List<FlattenedPathTreeChunk> Chunks);
 sealed record FlattenedPathTreeChunk(ulong? parentId, string? parentName, string[] childrenNames);
